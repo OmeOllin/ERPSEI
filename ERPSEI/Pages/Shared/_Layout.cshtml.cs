5abc0577f6e20294aa0fa3e752d99a057e33f3a7
Luis Linares <luis_linares75@hotmail.com>
2023-09-14 10:55:26 -0600

    Traducción del sitio a español y modificación del logo. Puesta en marcha del sitio.

---
+++ ERPSEI/Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERPSEI.Pages.Shared
+{
+    public class _LayoutModel : PageModel
+    {
+        private readonly ILogger<_LayoutModel> _logger;
+
+        public _LayoutModel(ILogger<_LayoutModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
