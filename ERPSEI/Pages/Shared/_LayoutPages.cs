8e041502443b36e690197d83c6a5b0fb309ea5a2
Luis Linares <luis_linares75@hotmail.com>
2023-09-26 17:14:23 -0600

    Se añade lógica para elementos activos / inactivos de submenus del nav principal.

--- ERPSEI/Pages/Shared/_LayoutPages.cs
+++ ERPSEI/Pages/Shared/_LayoutPages.cs
@@ -8,14 +8,35 @@ namespace ERPSEI.Pages.Shared
         public static string Inicio => "Inicio";
         public static string InicioNavClass(ViewContext viewContext) => PageNavClass(viewContext, Inicio);
 
+
         public static string AvisoPrivacidad => "AvisoPrivacidad";
         public static string AvisoPrivacidadNavClass(ViewContext viewContext) => PageNavClass(viewContext, AvisoPrivacidad);
+
 
         public static string Catalogos => "Catalogos";
         public static string CatalogosNavClass(ViewContext viewContext) => PageNavClass(viewContext, Catalogos);
 
+        public static string CatalogoPersonal => "CatalogoPersonal";
+        public static string CatalogoPersonalNavClass(ViewContext viewContext) => PageNavClass(viewContext, CatalogoPersonal);
+
+        public static string CatalogoPuestos => "CatalogoPuestos";
+        public static string CatalogoPuestosNavClass(ViewContext viewContext) => PageNavClass(viewContext, CatalogoPuestos);
+
+
         public static string ERP => "ERP";
         public static string ERPNavClass(ViewContext viewContext) => PageNavClass(viewContext, ERP);
+
+        public static string Organigrama => "Organigrama";
+        public static string OrganigramaNavClass(ViewContext viewContext) => PageNavClass(viewContext, Organigrama);
+
+        public static string Vacaciones => "Vacaciones";
+        public static string VacacionesNavClass(ViewContext viewContext) => PageNavClass(viewContext, Vacaciones);
+
+        public static string Incapacidades => "Incapacidades";
+        public static string IncapacidadesNavClass(ViewContext viewContext) => PageNavClass(viewContext, Incapacidades);
+
+        public static string Permisos => "Organigrama";
+        public static string PermisosNavClass(ViewContext viewContext) => PageNavClass(viewContext, Permisos);
 
 
         public static string PageNavClass(ViewContext viewContext, string page)
