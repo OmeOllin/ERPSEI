0a6137ba08aaf2c4460d947db5e96dad1ec51071
Luis Linares <luis_linares75@hotmail.com>
2023-09-26 10:45:38 -0600

    Se refactoriza la construcción y configuración de la aplicación.

--- ERPSEI/Program.cs
+++ ERPSEI/Program.cs
@@ -1,61 +1,21 @@
-using ERPSEI.Data;
-using ERPSEI.Email;
-using ERPSEI.Resources;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Host;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Localization;
-using System.Globalization;
-using System.Reflection;
+using ERPSEI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Obtiene la configuración del enviador de correos.
-IConfigurationSection emailSection = builder.Configuration.GetSection("Email");
-string address = (string)(emailSection.GetValue(typeof(string), "address") ?? throw new InvalidOperationException("Email 'address' not found."));
-string password = (string)(emailSection.GetValue(typeof(string), "password") ?? throw new InvalidOperationException("Email 'password' not found."));
-string smtp = (string)(emailSection.GetValue(typeof(string), "smtp") ?? throw new InvalidOperationException("Email 'smtp' not found."));
-int port = (int)(emailSection.GetValue(typeof(int), "port") ?? throw new InvalidOperationException("Email 'port' not found."));
+//Email configuration
+ServicesConfiguration.ConfigureEmail(builder);
 
-builder.Services.AddTransient<IEmailSender, EmailSender>( x =>
-    new EmailSender(address, password, smtp, port)
-);
+//Database configuration
+ServicesConfiguration.ConfigureDatabase(builder);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//Identity configuration
+ServicesConfiguration.ConfigureIdentity(builder);
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+//Pages and localization configuration
+ServicesConfiguration.ConfigurePagesAndLocalization(builder);
 
-builder.Services.AddRazorPages()
-    .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix)
-    .AddDataAnnotationsLocalization(options =>
-    {
-        options.DataAnnotationLocalizerProvider = (type, factory) =>
-        {
-            var assemblyName = new AssemblyName(typeof(ValidationsLocalization).GetTypeInfo().Assembly.FullName ?? "");
-            return factory.Create(nameof(ValidationsLocalization), assemblyName.Name ?? "");
-        };
-    });
-builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-builder.Services.Configure<RequestLocalizationOptions>(options => {
-    var supportedCultures = new[]
-    {
-        new CultureInfo("en-US"),
-        new CultureInfo("es-MX")
-    };
-
-    options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("es-MX");
-    options.SupportedUICultures = supportedCultures;
-    options.SupportedCultures = supportedCultures;
-});
-
-var app = builder.Build();
+//Build and run application
+WebApplication app = builder.Build();
 
 app.UseRequestLocalization();
 
@@ -72,6 +32,7 @@ else
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
---
+++ ERPSEI/ServicesConfiguration.cs
@@ -0,0 +1,69 @@
+﻿using ERPSEI.Data;
+using ERPSEI.Email;
+using ERPSEI.Resources;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using System.Reflection;
+
+namespace ERPSEI
+{
+    public static class ServicesConfiguration
+    {
+        public static void ConfigureEmail(WebApplicationBuilder _builder)
+        {
+            //Obtiene la configuración del enviador de correos.
+            IConfigurationSection emailSection = _builder.Configuration.GetSection("Email");
+            string address = (string)(emailSection.GetValue(typeof(string), "address") ?? throw new InvalidOperationException("Email 'address' not found."));
+            string password = (string)(emailSection.GetValue(typeof(string), "password") ?? throw new InvalidOperationException("Email 'password' not found."));
+            string smtp = (string)(emailSection.GetValue(typeof(string), "smtp") ?? throw new InvalidOperationException("Email 'smtp' not found."));
+            int port = (int)(emailSection.GetValue(typeof(int), "port") ?? throw new InvalidOperationException("Email 'port' not found."));
+
+            _builder.Services.AddTransient<IEmailSender, EmailSender>(x =>
+                new EmailSender(address, password, smtp, port)
+            );
+        }
+
+        public static void ConfigureDatabase(WebApplicationBuilder _builder)
+        {
+            var connectionString = _builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            _builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            _builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        }
+
+        public static void ConfigureIdentity(WebApplicationBuilder _builder)
+        {
+            _builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+        }
+
+        public static void ConfigurePagesAndLocalization(WebApplicationBuilder _builder)
+        {
+            _builder.Services.AddRazorPages()
+            .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix)
+            .AddDataAnnotationsLocalization(options =>
+            {
+                options.DataAnnotationLocalizerProvider = (type, factory) =>
+                {
+                    var assemblyName = new AssemblyName(typeof(ValidationsLocalization).GetTypeInfo().Assembly.FullName ?? "");
+                    return factory.Create(nameof(ValidationsLocalization), assemblyName.Name ?? "");
+                };
+            });
+            _builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+            _builder.Services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("es-MX")
+                };
+
+                options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("es-MX");
+                options.SupportedUICultures = supportedCultures;
+                options.SupportedCultures = supportedCultures;
+            });
+        }
+    }
+}
