ec31e3df3d2e3997f0835af977036563f11b1ebc
Luis Linares <luis_linares75@hotmail.com>
2023-10-17 12:52:32 -0600

    Se crea el catálogo de puestos

---
+++ ERPSEI/Areas/Catalogos/Pages/CatalogoPuestos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERPSEI.Areas.Catalogos.Pages
+{
+    public class CatalogoPuestosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
