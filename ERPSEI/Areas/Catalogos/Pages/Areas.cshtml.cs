dd38781da453855c12f09ad47ddf200f95c3e9d7
Luis Linares <luis_linares75@hotmail.com>
2023-10-17 17:15:30 -0600

    Se agrega el esqueleto del módulo Catálogo de Áreas

---
+++ ERPSEI/Areas/Catalogos/Pages/Areas.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERPSEI.Areas.Catalogos.Pages
+{
+    public class AreasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
