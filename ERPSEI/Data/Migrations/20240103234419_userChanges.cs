f4bad86d9ceb81bfd4e00c162f54fa3e58691c90
Luis Linares <luis_linares75@hotmail.com>
2024-01-04 09:33:37 -0600

    Migración para establecer la referencia a userID como string en la entidad Empleado.

---
+++ ERPSEI/Data/Migrations/20240103234419_userChanges.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class userChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_EmpleadoId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Empleados",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_EmpleadoId",
+                table: "AspNetUsers",
+                column: "EmpleadoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_EmpleadoId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Empleados",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_EmpleadoId",
+                table: "AspNetUsers",
+                column: "EmpleadoId",
+                unique: true,
+                filter: "[EmpleadoId] IS NOT NULL");
+        }
+    }
+}
