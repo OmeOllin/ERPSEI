17b8b7a084b5307fed7dabe6994778304bd772f2
Luis Linares <luis_linares75@hotmail.com>
2024-01-08 12:04:42 -0600

    Se modifica el generador de passwords para que incluya un caracter especial

--- ERPSEI/Data/Entities/AppUserManager.cs
+++ ERPSEI/Data/Entities/AppUserManager.cs
@@ -38,12 +38,14 @@ namespace ERPSEI.Data.Entities
 			int half = size / 2;
 
 			StringBuilder builder = new StringBuilder();
-			builder.Append(RandomString(half, true));
-			builder.Append(RandomNumber(1000, 9999));
 			builder.Append(RandomString(half, false));
+			builder.Append(RandomNumber(1000, 9999));
+			builder.Append(RandomString(half - 1, true));
+			builder.Append(RandomSymbol());
 			return builder.ToString();
 		}
 
+		//Genera un número aleatorio de un rango dado.
         private string RandomNumber(int min, int max)
         {
 			// Generate a random number
@@ -57,7 +59,7 @@ namespace ERPSEI.Data.Entities
 
 		//Genera un string aleatorio con el tamaño y el case dados.
 		//Si el segundo parámetro es verdadero, el string resultante será lowercase.
-		public string RandomString(int size, bool lowerCase)
+		private string RandomString(int size, bool lowerCase)
 		{
 			StringBuilder builder = new StringBuilder();
 			Random random = new Random();
@@ -71,6 +73,23 @@ namespace ERPSEI.Data.Entities
 				return builder.ToString().ToLower();
 			return builder.ToString();
 		}
+
+		private string RandomSymbol()
+		{
+			// Caracteres especiales permitidos
+			string validChars = "!@#$%&*?";
+			Random random = new Random();
+
+			//Selecciona un caracter especial aleatorio
+			char[] chars = new char[1];
+			for (int i = 0; i < 1; i++)
+			{
+				chars[i] = validChars[random.Next(0, validChars.Length)];
+			}
+			return new string(chars);
+		}
+
+
 
 	}
 }
