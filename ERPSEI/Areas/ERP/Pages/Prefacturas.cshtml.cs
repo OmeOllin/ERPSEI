bb55ba79ad0982ce488bda30b0b0a4b704adeb35
Luis Linares <luis_linares75@hotmail.com>
2024-02-22 12:44:01 -0600

    Se prepara la primer versión de prefacturas

--- ERPSEI/Areas/ERP/Pages/Prefacturas.cshtml.cs
+++ ERPSEI/Areas/ERP/Pages/Prefacturas.cshtml.cs
@@ -1,27 +1,102 @@
+using ERPSEI.Data.Entities.Empresas;
+using ERPSEI.Data.Managers.Empresas;
+using ERPSEI.Requests;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Localization;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ERPSEI.Areas.ERP.Pages
 {
-    public class PrefacturasModel : PageModel
+	[Authorize(Roles = $"{ServicesConfiguration.RolMaster}, {ServicesConfiguration.RolAdministrador}")]
+	public class PrefacturasModel : PageModel
     {
+		private readonly IEmpresaManager _empresaManager;
         private readonly IStringLocalizer<PrefacturasModel> _strLocalizer;
         private readonly ILogger<PrefacturasModel> _logger;
 
+		[BindProperty]
+		public EmpresaModel InputEmpresa { get; set; }
+
+		public class EmpresaModel()
+        {
+            
+            [DataType(DataType.Text)]
+            [DisplayName("SearchCompanyField")]
+			[StringLength(30, ErrorMessage = "FieldLength", MinimumLength = 2)]
+			public string Texto {  get; set; } = string.Empty;
+        }
+        
         public PrefacturasModel(
+			IEmpresaManager empresaManager,
             IStringLocalizer<PrefacturasModel> stringLocalizer,
             ILogger<PrefacturasModel> logger
         )
         {
+			_empresaManager = empresaManager;
             _strLocalizer = stringLocalizer;
             _logger = logger;
+
+            InputEmpresa = new EmpresaModel();
         }
 
         public void OnGet()
         {
         }
-        public void OnPost()
+
+        public async Task<JsonResult> OnPostGetEmpresaSuggestion(string texto, string idempresa)
         {
-        }
-    }
+			ServerResponse resp = new ServerResponse(true, _strLocalizer["ConsultadoUnsuccessfully"]);
+			try
+			{
+				int idEmp = 0;
+
+				if (!int.TryParse(idempresa, out idEmp)) { idEmp = 0; }
+
+				resp.Datos = await GetEmpresasSuggestion(texto, idEmp);
+				resp.TieneError = false;
+				resp.Mensaje = _strLocalizer["ConsultadoSuccessfully"];
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex.Message);
+			}
+
+			return new JsonResult(resp);
+		}
+		private async Task<string> GetEmpresasSuggestion(string texto, int idempresa)
+		{
+			string jsonResponse;
+			List<string> jsonEmpresas = new List<string>();
+
+			List<Empresa> empresas = await _empresaManager.GetAllAsync();
+			empresas = empresas.Where(e => e.RazonSocial.ToLowerInvariant().Contains(texto.ToLowerInvariant()) || e.RFC.ToLowerInvariant().Contains(texto.ToLowerInvariant())).ToList();
+
+			if (empresas != null)
+			{
+				foreach (Empresa e in empresas)
+				{
+					//Si viene establecido el id empresa, omite el elemento con ese id.
+					if (idempresa >= 1 && e.Id == idempresa) { continue; }
+
+					jsonEmpresas.Add($"{{" +
+										$"\"id\": {e.Id}, " +
+										$"\"value\": \"{e.RazonSocial}\", " +
+										$"\"label\": \"{e.RFC} - {e.RazonSocial}\", " +
+										$"\"rfc\": \"{e.RFC}\", " +
+										$"\"razonSocial\": \"{e.RazonSocial}\", " +
+										$"\"objetoSocial\": \"{e.ObjetoSocial}\", " +
+										$"\"actividadEconomica\": \"{e.ActividadEconomica?.Nombre}\", " +
+										$"\"domicilioFiscal\": \"{e.DomicilioFiscal}\"" +
+									$"}}");
+				}
+			}
+
+			jsonResponse = $"[{string.Join(",", jsonEmpresas)}]";
+
+			return jsonResponse;
+		}
+	}
 }
--- ERPSEI/Areas/ERP/Pages/Prefacturas.cshtml.js
+++ ERPSEI/Areas/ERP/Pages/Prefacturas.cshtml.js
@@ -1,1 +1,109 @@
 ﻿const postOptions = { headers: { "RequestVerificationToken": $('input[name="__RequestVerificationToken"]').val() } }
+
+document.addEventListener('DOMContentLoaded', function () {
+    autoCompletar("#inpEmisor", {
+        select: function (element, item) { toggleEmisorInfo(item); },
+        change: function (element, item) {
+            let inpEmisor = document.getElementById('inpEmisor');
+            if ((inpEmisor.value || "").length <= 0) { toggleEmisorInfo(); }
+        }
+    });
+    autoCompletar("#inpReceptor", {
+        select: function (element, item) { toggleReceptorInfo(item); },
+        change: function (element, item) {
+            let inpReceptor = document.getElementById('inpReceptor');
+            if ((inpReceptor.value || "").length <= 0) { toggleReceptorInfo(); }
+        }
+    });
+});
+
+//Función para mostrar u ocultar la información del emisor. Si se establece el parámetro item, se muestra. De lo contrario, se oculta.
+function toggleEmisorInfo(item = null) {
+    let divFilledInfoEmisor = document.getElementById("divFilledInfoEmisor");
+    let divEmptyInfoEmisor = document.getElementById("divEmptyInfoEmisor");
+
+    let lblRFC = document.getElementById("lblRFCEmisor");
+    let lblRazonSocial = document.getElementById("lblRazonSocialEmisor");
+    let lblObjetoSocial = document.getElementById("lblObjetoSocialEmisor");
+    let lblActividadEconomica = document.getElementById("lblActividadEconomicaEmisor");
+    let lblDomicilioFiscal = document.getElementById("lblDomicilioFiscalEmisor");
+
+    if (item != null) {
+        lblRFC.innerHTML = item.rfc;
+        lblRazonSocial.innerHTML = item.razonSocial;
+        lblObjetoSocial.innerHTML = item.objetoSocial;
+        lblActividadEconomica.innerHTML = item.actividadEconomica;
+        lblDomicilioFiscal.innerHTML = item.domicilioFiscal;
+
+        divEmptyInfoEmisor.classList.remove('d-flex');
+        divEmptyInfoEmisor.setAttribute('hidden', true);
+        divFilledInfoEmisor.removeAttribute('hidden');
+
+        $("#inpReceptor").data('idempresa', item.id);
+    }
+    else {
+        lblRFC.innerHTML = emptyInfo;
+        lblRazonSocial.innerHTML = emptyInfo;
+        lblObjetoSocial.innerHTML = emptyInfo;
+        lblActividadEconomica.innerHTML = emptyInfo;
+        lblDomicilioFiscal.innerHTML = emptyInfo;
+
+        divEmptyInfoEmisor.removeAttribute('hidden');
+        divFilledInfoEmisor.setAttribute('hidden', true);
+        divEmptyInfoEmisor.classList.add('d-flex');
+
+        $("#inpReceptor").data('idempresa', null);
+    }
+
+}
+
+//Función para mostrar u ocultar la información del receptor. Si se establece el parámetro item, se muestra. De lo contrario, se oculta.
+function toggleReceptorInfo(item = null) {
+    let divFilledInfoReceptor = document.getElementById("divFilledInfoReceptor");
+    let divEmptyInfoReceptor = document.getElementById("divEmptyInfoReceptor");
+
+    let lblRFC = document.getElementById("lblRFCReceptor");
+    let lblRazonSocial = document.getElementById("lblRazonSocialReceptor");
+    let lblObjetoSocial = document.getElementById("lblObjetoSocialReceptor");
+    let lblActividadEconomica = document.getElementById("lblActividadEconomicaReceptor");
+    let lblDomicilioFiscal = document.getElementById("lblDomicilioFiscalReceptor");
+
+    if (item != null) {
+        lblRFC.innerHTML = item.rfc;
+        lblRazonSocial.innerHTML = item.razonSocial;
+        lblObjetoSocial.innerHTML = item.objetoSocial;
+        lblActividadEconomica.innerHTML = item.actividadEconomica;
+        lblDomicilioFiscal.innerHTML = item.domicilioFiscal;
+
+        divEmptyInfoReceptor.classList.remove('d-flex');
+        divEmptyInfoReceptor.setAttribute('hidden', true);
+        divFilledInfoReceptor.removeAttribute('hidden');
+
+        $("#inpEmisor").data('idempresa', item.id);
+    }
+    else {
+        lblRFC.innerHTML = emptyInfo;
+        lblRazonSocial.innerHTML = emptyInfo;
+        lblObjetoSocial.innerHTML = emptyInfo;
+        lblActividadEconomica.innerHTML = emptyInfo;
+        lblDomicilioFiscal.innerHTML = emptyInfo;
+
+        divEmptyInfoReceptor.removeAttribute('hidden');
+        divFilledInfoReceptor.setAttribute('hidden', true);
+        divEmptyInfoReceptor.classList.add('d-flex');
+
+        $("#inpEmisor").data('idempresa', null);
+    }
+
+}
+
+//Función para capturar el clic del botón validar. Valida que se pueda efectuar una factura entre el emisor y el receptor.
+function onValidarClick() {
+   
+}
+
+//Función para capturar el clic del botón limpiar. Limpia y oculta la información del emisor y del receptor.
+function onLimpiarClick() {
+    toggleEmisorInfo();
+    toggleReceptorInfo();
+}
