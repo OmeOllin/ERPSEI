29c8eb640a7706b6961d9bf9a4e7563c86b2f8a9
Luis Linares <luis_linares75@hotmail.com>
2024-02-28 10:25:45 -0600

    Se agrega a base de datos, el campo de Clave a las actividades económicas

---
+++ ERPSEI/Data/Migrations/20240228161239_cambiosActividadEmpresarial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class cambiosActividadEmpresarial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Clave",
+                table: "ActividadesEconomicas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Clave",
+                table: "ActividadesEconomicas");
+        }
+    }
+}
