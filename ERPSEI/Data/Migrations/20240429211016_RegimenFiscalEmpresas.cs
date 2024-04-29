7098e80c1df8906a86d57378f2f5634d3f6f9dbe
Luis Linares <luis_linares75@hotmail.com>
2024-04-29 15:29:11 -0600

    Se agrega migración de base de datos para añadir campo de régimen fiscal de empresas

---
+++ ERPSEI/Data/Migrations/20240429211016_RegimenFiscalEmpresas.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RegimenFiscalEmpresas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RegimenFiscalId",
+                table: "Empresas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empresas_RegimenFiscalId",
+                table: "Empresas",
+                column: "RegimenFiscalId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Empresas_RegimenesFiscales_RegimenFiscalId",
+                table: "Empresas",
+                column: "RegimenFiscalId",
+                principalTable: "RegimenesFiscales",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Empresas_RegimenesFiscales_RegimenFiscalId",
+                table: "Empresas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Empresas_RegimenFiscalId",
+                table: "Empresas");
+
+            migrationBuilder.DropColumn(
+                name: "RegimenFiscalId",
+                table: "Empresas");
+        }
+    }
+}
