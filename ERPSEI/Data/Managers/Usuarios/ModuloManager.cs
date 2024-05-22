71602e43daa11379f1e0c92510d3c51d97450a38
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-05-22 16:35:59 -0600

    Se crea la clase e interfaz manager para los módulos.

---
+++ ERPSEI/Data/Managers/Usuarios/ModuloManager.cs
@@ -0,0 +1,31 @@
+﻿using ERPSEI.Data.Entities.Usuarios;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERPSEI.Data.Managers.Usuarios
+{
+    public class ModuloManager : IModuloManager
+	{
+        ApplicationDbContext db { get; set; }
+
+        public ModuloManager(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
+		public async Task<List<Modulo>> GetAllAsync()
+		{
+			return await db.Modulos.ToListAsync();
+		}
+
+		public async Task<Modulo?> GetByIdAsync(int id)
+        {
+            return await db.Modulos.Where(e => e.Id == id).FirstOrDefaultAsync();
+        }
+
+		public async Task<Modulo?> GetByNameAsync(string name)
+		{
+			return await db.Modulos.Where(c => c.Nombre == name.ToLower()).FirstOrDefaultAsync();
+		}
+
+	}
+}
