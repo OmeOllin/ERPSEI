1ac524d293f20095ec2a6d54f52ae9b115a7b5a1
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-05-22 18:00:02 -0600

    Se elimina referencia a AccesoModulo

--- ERPSEI/Data/Entities/Usuarios/AppUser.cs
+++ ERPSEI/Data/Entities/Usuarios/AppUser.cs
@@ -16,7 +16,5 @@ namespace ERPSEI.Data.Entities.Usuarios
         public ICollection<Prefactura> PrefacturasCreadas { get; set; } = new List<Prefactura>();
         public ICollection<Prefactura> PrefacturasAutorizadas { get; set; } = new List<Prefactura>();
         public ICollection<Prefactura> PrefacturasFinalizadas { get; set; } = new List<Prefactura>();
-
-        public ICollection<AccesoModulo> Accesos { get; set; } = new List<AccesoModulo>();
     }
 }
