3f0382bbd8d37ba820353d72feea12eda3264eac
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-05-22 18:33:32 -0600

    Se modifica el método GetAllAsync para que traiga el conjunto de accesos asociados a un Rol

--- ERPSEI/Data/Managers/Usuarios/AppRoleManager.cs
+++ ERPSEI/Data/Managers/Usuarios/AppRoleManager.cs
@@ -6,24 +6,30 @@ namespace ERPSEI.Data.Managers.Usuarios
 {
 	public class AppRoleManager : RoleManager<AppRole>
     {
-        public AppRoleManager(
+		ApplicationDbContext _db { get; set; }
+
+		public AppRoleManager(
             IRoleStore<AppRole> store,
             IEnumerable<IRoleValidator<AppRole>> roleValidators,
             ILookupNormalizer keyNormalizer,
             IdentityErrorDescriber errors,
-            ILogger<RoleManager<AppRole>> logger) :
+            ILogger<RoleManager<AppRole>> logger,
+            ApplicationDbContext db) :
             base(store,
                 roleValidators,
                 keyNormalizer,
                 errors,
                 logger)
         {
+            _db = db;
         }
 
         public async Task<List<AppRole>> GetAllAsync()
         {
 
-            return await Roles.ToListAsync();
+            return await _db.Roles
+                .Include(r => r.Accesos)
+                .ToListAsync();
         }
     }
 }
