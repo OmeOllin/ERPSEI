d08929c6cbc78cb2abf6b468269a0b7ca5d091c7
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-05-27 18:43:26 -0600

    Se agrega método ConfigureAuthorization para encapsular el procedimiento de configuración de autorización. Se comienza a experimentar el añadido de políticas al sistema de autorizacion de acceso a módulos.

--- ERPSEI/Program.cs
+++ ERPSEI/Program.cs
@@ -1,7 +1,4 @@
 using ERPSEI;
-using ERPSEI.Data.Entities.Usuarios;
-using ERPSEI.Data.Managers.Usuarios;
-using ERPSEI.Email;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,40 +17,10 @@ ServicesConfiguration.ConfigurePagesAndLocalization(builder);
 //Build and run application
 WebApplication app = builder.Build();
 
+//Authorization configuration
 using(IServiceScope scope = app.Services.CreateScope())
 {
-    //Se inicializan los roles
-    AppRoleManager roleManager = scope.ServiceProvider.GetRequiredService<AppRoleManager>();
-
-    //Este rol debe existir siempre para poder tener acceso completo al sistema.
-    if (!await roleManager.RoleExistsAsync(ServicesConfiguration.RolMaster)) { await roleManager.CreateAsync(new AppRole() { Name = ServicesConfiguration.RolMaster }); }
-	//Este rol debe existir siempre para poder ascender a los candidatos a usuarios con los privilegios más básicos.
-	if (!await roleManager.RoleExistsAsync(ServicesConfiguration.RolUsuario)) { await roleManager.CreateAsync(new AppRole() { Name = ServicesConfiguration.RolUsuario }); }
-	//Este rol debe existir siempre para poder admitir registro de empleados candidatos.
-	if (!await roleManager.RoleExistsAsync(ServicesConfiguration.RolCandidato)) { await roleManager.CreateAsync(new AppRole() { Name = ServicesConfiguration.RolCandidato }); }
-
-	ServicesConfiguration.Roles = await roleManager.GetAllAsync();
-
-    //Se inicializa el usuario master
-    AppUserManager userManager = scope.ServiceProvider.GetRequiredService<AppUserManager>();
-
-    if (await userManager.FindByEmailAsync(ServicesConfiguration.MasterUser.Email ?? "") == null)
-    {
-        //Genera password para usuario master
-        ServicesConfiguration.MasterPassword = userManager.GenerateRandomPassword(10);
-        //Crea al usuario master.
-        var result = await userManager.CreateAsync(ServicesConfiguration.MasterUser, ServicesConfiguration.MasterPassword);
-
-        if (result.Succeeded)
-        {
-            //Asigna el rol de Master al usuario master.
-            await userManager.AddToRoleAsync(ServicesConfiguration.MasterUser, ServicesConfiguration.RolMaster);
-
-            //Envía password por correo para notificarlo.
-            IEmailSender emailSender = scope.ServiceProvider.GetRequiredService<IEmailSender>();
-            emailSender.SendEmailAsync(ServicesConfiguration.MasterUser.Email ?? "", "Login Password", $"Use this password to login: {ServicesConfiguration.MasterPassword}");
-        }
-    }
+    ServicesConfiguration.ConfigureAuthorization(scope, builder);
 }
 
 app.UseSession();
