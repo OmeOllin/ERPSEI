f115ac943ce5065884b5e0179617e5d3a64f2d02
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-05-27 18:42:35 -0600

    Se agregan constructores faltantes de la entidad AppRole

--- ERPSEI/Data/Entities/Usuarios/AppRole.cs
+++ ERPSEI/Data/Entities/Usuarios/AppRole.cs
@@ -4,6 +4,10 @@ namespace ERPSEI.Data.Entities.Usuarios
 {
 	public class AppRole : IdentityRole
     {
+		public AppRole():base() { }
+
+		public AppRole(string roleName):base(roleName) { }
+
 		public ICollection<AccesoModulo> Accesos { get; set; } = new List<AccesoModulo>();
     }
 }
