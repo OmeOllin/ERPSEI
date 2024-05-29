5ab69698db999c3778a816576a21d738a198a1c5
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-05-29 13:44:12 -0600

    Se crean dos clases para el manejo de las autorizaciones por módulo en el sistema

---
+++ ERPSEI/Authorization/AccessHandler.cs
@@ -0,0 +1,37 @@
+﻿using ERPSEI.Data.Entities.Usuarios;
+using ERPSEI.Data.Managers.Usuarios;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ERPSEI.Authorization
+{
+	public class AccessHandler : AuthorizationHandler<AccessRequirement>
+	{
+		readonly IModuloManager _moduloManager;
+
+		public AccessHandler(IModuloManager moduloManager) 
+		{ 
+			_moduloManager = moduloManager;
+		}
+
+		protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, AccessRequirement requirement)
+		{
+			Modulo? m = await _moduloManager.GetByNameAsync(requirement.ModuleName??string.Empty);
+
+            if (m != null)
+			{
+				bool succeeded = false;
+
+				foreach (AccesoModulo acceso in m.Accesos)
+				{
+					if (context.User.IsInRole(acceso.Rol?.Name ?? ""))
+					{
+						succeeded = acceso.PuedeConsultar == 1;
+						break;
+					}
+				}
+
+				if (succeeded) { context.Succeed(requirement); }
+			}
+		}
+	}
+}
