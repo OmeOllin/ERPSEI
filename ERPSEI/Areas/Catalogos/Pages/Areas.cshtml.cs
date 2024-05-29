d5958085d00d2ad9e6bbfc66d965eb13e4351cf5
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-05-29 13:45:16 -0600

    Se configura la autorización de acceso a los módulos en base a roles iniciando con el módulo de Areas para realizar pruebas

--- ERPSEI/Areas/Catalogos/Pages/Areas.cshtml.cs
+++ ERPSEI/Areas/Catalogos/Pages/Areas.cshtml.cs
@@ -12,7 +12,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ERPSEI.Areas.Catalogos.Pages
 {
-    [Authorize(Roles = $"{ServicesConfiguration.RolMaster}, {ServicesConfiguration.RolAdministrador}")]
+    [Authorize(Policy = "AreasPolicy")]
     public class AreasModel : PageModel
     {
         private readonly ApplicationDbContext _db;
