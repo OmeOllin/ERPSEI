7e6cf2da5952b8f2d80fa7bdce831304e13246c0
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-05-29 13:44:12 -0600

    Se crean dos clases para el manejo de las autorizaciones por módulo en el sistema

---
+++ ERPSEI/Authorization/AccessRequirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace ERPSEI.Authorization
+{
+	public class AccessRequirement : IAuthorizationRequirement
+	{
+		public string? ModuleName { get; }
+
+		public AccessRequirement(string moduleName)
+		{
+			ModuleName = moduleName;
+		}
+	}
+}
