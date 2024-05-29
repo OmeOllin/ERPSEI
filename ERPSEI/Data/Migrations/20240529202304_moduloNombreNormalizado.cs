06b49acf9317c4092f35518f7561fa1cf202c725
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-05-29 14:24:39 -0600

    Se agrega migración para columna de nombre normalizado de módulo

---
+++ ERPSEI/Data/Migrations/20240529202304_moduloNombreNormalizado.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class moduloNombreNormalizado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NombreNormalizado",
+                table: "Modulos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "NombreNormalizado",
+                value: "gestiondetalento");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "NombreNormalizado",
+                value: "usuarios");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "NombreNormalizado",
+                value: "puestos");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "NombreNormalizado",
+                value: "areas");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "NombreNormalizado",
+                value: "subareas");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "NombreNormalizado",
+                value: "oficinas");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "NombreNormalizado",
+                value: "empresas");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "NombreNormalizado",
+                value: "origenes");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "NombreNormalizado",
+                value: "niveles");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "NombreNormalizado",
+                value: "perfiles");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "NombreNormalizado",
+                value: "vacaciones");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "NombreNormalizado",
+                value: "incapacidades");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "NombreNormalizado",
+                value: "permisos");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "NombreNormalizado",
+                value: "prefacturas");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "NombreNormalizado",
+                value: "organigrama");
+
+            migrationBuilder.UpdateData(
+                table: "Modulos",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "NombreNormalizado",
+                value: "asistencia");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NombreNormalizado",
+                table: "Modulos");
+        }
+    }
+}
