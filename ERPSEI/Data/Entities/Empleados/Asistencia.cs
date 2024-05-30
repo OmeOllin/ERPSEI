e3567f0754c4c84460e6c77f84aaa9c57ed8a4f3
JorgeAlbertoCruzPercastegui <jacp@soportecliente.com.mx>
2024-05-29 18:41:30 -0600

    Se agrega Entidades Asistencia y Empleado en asistencia.

--- ERPSEI/Data/Entities/Empleados/Asistencia.cs
+++ ERPSEI/Data/Entities/Empleados/Asistencia.cs
@@ -1,12 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ERPSEI.Data.Managers;
+using ERPSEI.Data.Managers.Empleados;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ERPSEI.Data.Entities.Empleados
 {
-	public class Asistencias
+	public class Asistencia 
 	{
 		[DatabaseGenerated(DatabaseGeneratedOption.None)]
-		public int? Id { get; set; }
+		public int Id { get; set; }
 		public string? Nombre { get; set; } = string.Empty;
 		public DateOnly? Fecha { get; set; }
 		public TimeOnly? HoraEntrada { get; set; }
@@ -14,5 +16,13 @@ namespace ERPSEI.Data.Entities.Empleados
 		public int? Retardo { get; set; }
 		public int? Total { get; set; }
 		public int? Faltas { get; set; }
+
+		//Relaciones de la entidad
+		public int? EmpleadoId { get; set; }
+		public Empleado? Empleado { get; set; }
+
+		//[NotMapped]
+		//public List<Empleado>? Empleados { get; set; }
+		//public ICollection<Empleado>? Empleados { get; set; }
 	}
 }
--- ERPSEI/Data/Entities/Empleados/Empleado.cs
+++ ERPSEI/Data/Entities/Empleados/Empleado.cs
@@ -2,72 +2,73 @@
 
 namespace ERPSEI.Data.Entities.Empleados
 {
-    public class Empleado
-    {
+	public class Empleado
+	{
 		[DatabaseGenerated(DatabaseGeneratedOption.None)]
 		public int Id { get; set; }
 
 		public string Nombre { get; set; } = string.Empty;
 
-        public string NombrePreferido { get; set; } = string.Empty;
+		public string NombrePreferido { get; set; } = string.Empty;
 
-        public string ApellidoPaterno { get; set; } = string.Empty;
+		public string ApellidoPaterno { get; set; } = string.Empty;
 
-        public string ApellidoMaterno { get; set; } = string.Empty;
+		public string ApellidoMaterno { get; set; } = string.Empty;
 
-        public string NombreCompleto { get; set; } = string.Empty;
+		public string NombreCompleto { get; set; } = string.Empty;
 
 		public DateTime FechaIngreso { get; set; }
 
 		public DateTime FechaNacimiento { get; set; }
 
-        public string Direccion { get; set; } = string.Empty;
+		public string Direccion { get; set; } = string.Empty;
 
-        public string Telefono { get; set; } = string.Empty;
+		public string Telefono { get; set; } = string.Empty;
 
-        public string Email { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
 
-        public string CURP { get; set; } = string.Empty;
+		public string CURP { get; set; } = string.Empty;
 
-        public string RFC {  get; set; } = string.Empty;
+		public string RFC { get; set; } = string.Empty;
 
-        public string NSS { get; set; } = string.Empty;
+		public string NSS { get; set; } = string.Empty;
 
-        public int Deshabilitado { get; set; } = 0;
+		public int Deshabilitado { get; set; } = 0;
 
-        
-        //Relaciones de la entidad
-        public int? GeneroId {  get; set; }
+
+		//Relaciones de la entidad
+		public int? GeneroId { get; set; }
 		public Genero? Genero { get; set; }
 
-        public int? SubareaId { get; set; }
-        public Subarea? Subarea { get; set; }
+		public int? SubareaId { get; set; }
+		public Subarea? Subarea { get; set; }
 
-        public int? OficinaId { get; set; }
-        public Oficina? Oficina { get; set; }
+		public int? OficinaId { get; set; }
+		public Oficina? Oficina { get; set; }
 
-        public int? PuestoId { get; set; }
+		public int? PuestoId { get; set; }
 		public Puesto? Puesto { get; set; }
-		//public Asistencia? Asistencia { get; set; }
 
 		public int? AreaId { get; set; }
 		public Area? Area { get; set; }
 
-        public int? EstadoCivilId {  get; set; }
+		public int? EstadoCivilId { get; set; }
 		public EstadoCivil? EstadoCivil { get; set; }
 
-        public string? UserId { get; set; } = string.Empty;
+		public string? UserId { get; set; } = string.Empty;
 
 		public ICollection<ContactoEmergencia>? ContactosEmergencia { get; }
 
-        public ICollection<ArchivoEmpleado>? ArchivosEmpleado { get; }
+		public ICollection<ArchivoEmpleado>? ArchivosEmpleado { get; }
+		
+		public ICollection<Asistencia>? Asistencias { get; set; }
 
-        //Referencia a esta misma tabla para el jefe del empleado
+		//Referencia a esta misma tabla para el jefe del empleado
 		public int? JefeId { get; set; }
 
-        //Empleados de los que el empleado es jefe
-        [NotMapped]
-        public List<Empleado>? Empleados { get; set; }
+		//Empleados de los que el empleado es jefe
+		[NotMapped]
+		public List<Empleado>? Empleados { get; set; }
 
 	}
 }
