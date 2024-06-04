785bc6e4b616ef22f8193d59775adced53e73887
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-06-04 10:19:02 -0600

    Se configura inyección de dependencias para el administrador de accesos a módulos

--- ERPSEI/ServicesConfiguration.cs
+++ ERPSEI/ServicesConfiguration.cs
@@ -59,26 +59,27 @@ namespace ERPSEI
             _builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
             _builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            _builder.Services.AddScoped<AppRoleManager, AppRoleManager>();
+			_builder.Services.AddScoped<IAccesoModuloManager, AccesoModuloManager>();
+			_builder.Services.AddScoped<AppRoleManager, AppRoleManager>();
             _builder.Services.AddScoped<IModuloManager, ModuloManager>();
 
             _builder.Services.AddScoped<IConceptoManager, ConceptoManager>();
             _builder.Services.AddScoped<IPrefacturaManager, PrefacturaManager>();
             _builder.Services.AddScoped<IExportacionManager, ExportacionManager>();
-			_builder.Services.AddScoped <IFormaPagoManager, FormaPagoManager>();
-			_builder.Services.AddScoped <IImpuestoManager, ImpuestoManager>();
-			_builder.Services.AddScoped <IMesManager, MesManager>();
-			_builder.Services.AddScoped <IMetodoPagoManager, MetodoPagoManager>();
+			_builder.Services.AddScoped<IFormaPagoManager, FormaPagoManager>();
+			_builder.Services.AddScoped<IImpuestoManager, ImpuestoManager>();
+			_builder.Services.AddScoped<IMesManager, MesManager>();
+			_builder.Services.AddScoped<IMetodoPagoManager, MetodoPagoManager>();
             _builder.Services.AddScoped<IMonedaManager, MonedaManager>();
-			_builder.Services.AddScoped <IObjetoImpuestoManager, ObjetoImpuestoManager>();
-			_builder.Services.AddScoped <IPeriodicidadManager, PeriodicidadManager>();
-			_builder.Services.AddScoped <IRegimenFiscalManager, RegimenFiscalManager>();
-			_builder.Services.AddScoped <ITasaOCuotaManager, TasaOCuotaManager>();
-			_builder.Services.AddScoped <ITipoComprobanteManager, TipoComprobanteManager>();
-			_builder.Services.AddScoped <ITipoFactorManager, TipoFactorManager>();
-			_builder.Services.AddScoped <ITipoRelacionManager, TipoRelacionManager>();
-			_builder.Services.AddScoped <IUnidadMedidaManager, UnidadMedidaManager>();
-			_builder.Services.AddScoped <IUsoCFDIManager, UsoCFDIManager>();
+			_builder.Services.AddScoped<IObjetoImpuestoManager, ObjetoImpuestoManager>();
+			_builder.Services.AddScoped<IPeriodicidadManager, PeriodicidadManager>();
+			_builder.Services.AddScoped<IRegimenFiscalManager, RegimenFiscalManager>();
+			_builder.Services.AddScoped<ITasaOCuotaManager, TasaOCuotaManager>();
+			_builder.Services.AddScoped<ITipoComprobanteManager, TipoComprobanteManager>();
+			_builder.Services.AddScoped<ITipoFactorManager, TipoFactorManager>();
+			_builder.Services.AddScoped<ITipoRelacionManager, TipoRelacionManager>();
+			_builder.Services.AddScoped<IUnidadMedidaManager, UnidadMedidaManager>();
+			_builder.Services.AddScoped<IUsoCFDIManager, UsoCFDIManager>();
 			_builder.Services.AddScoped<IProductoServicioManager, ProductoServicioManager>();
 			_builder.Services.AddScoped<IRWCatalogoManager<ActividadEconomica>, ActividadEconomicaManager>();
 
