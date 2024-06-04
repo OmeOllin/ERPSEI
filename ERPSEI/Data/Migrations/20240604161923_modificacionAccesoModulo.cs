4de8d72d035ad59614bc29d9591e9ce5271a6331
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-06-04 10:20:38 -0600

    Se agrega migración para incluir campo PuedeAutorizar a la entidad AccesoModulo

---
+++ ERPSEI/Data/Migrations/20240604161923_modificacionAccesoModulo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class modificacionAccesoModulo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PuedeAutorizar",
+                table: "AccesosModulos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PuedeAutorizar",
+                table: "AccesosModulos");
+        }
+    }
+}
