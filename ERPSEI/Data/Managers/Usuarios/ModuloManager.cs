cf8e6582dc4e50c08c95992269f3043eabdd48aa
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-06-04 12:52:07 -0600

    Se incluye la entidad rol a la consulta de módulos

--- ERPSEI/Data/Managers/Usuarios/ModuloManager.cs
+++ ERPSEI/Data/Managers/Usuarios/ModuloManager.cs
@@ -14,17 +14,17 @@ namespace ERPSEI.Data.Managers.Usuarios
 
 		public async Task<List<Modulo>> GetAllAsync()
 		{
-			return await db.Modulos.Include(m => m.Accesos).ToListAsync();
+			return await db.Modulos.Include(m => m.Accesos).ThenInclude(a => a.Rol).ToListAsync();
 		}
 
 		public async Task<Modulo?> GetByIdAsync(int id)
         {
-            return await db.Modulos.Include(m => m.Accesos).Where(e => e.Id == id).FirstOrDefaultAsync();
+            return await db.Modulos.Include(m => m.Accesos).ThenInclude(a => a.Rol).Where(e => e.Id == id).FirstOrDefaultAsync();
         }
 
 		public async Task<Modulo?> GetByNameAsync(string name)
 		{
-			return await db.Modulos.Include(m => m.Accesos).Where(c => c.NombreNormalizado.ToLower() == name.ToLower()).FirstOrDefaultAsync();
+			return await db.Modulos.Include(m => m.Accesos).ThenInclude(a => a.Rol).Where(c => c.NombreNormalizado.ToLower() == name.ToLower()).FirstOrDefaultAsync();
 		}
 
 	}
