5a489971603888dd44ae1dc2692a1f795aa7c9b2
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-06-13 00:30:36 -0600

    Se crea y ejecuta migración para integrar el campo PuedeTodo a la base de datos

---
+++ ERPSEI/Data/Migrations/20240613062516_campoPuedeTodoAccesoModulo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class campoPuedeTodoAccesoModulo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PuedeTodo",
+                table: "AccesosModulos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PuedeTodo",
+                table: "AccesosModulos");
+        }
+    }
+}
