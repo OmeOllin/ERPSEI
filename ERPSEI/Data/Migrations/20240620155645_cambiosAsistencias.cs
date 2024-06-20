29d308e4e46a539e37ba2944b68dbf989d88df97
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-06-20 10:05:55 -0600

    Migración de Jorge

---
+++ ERPSEI/Data/Migrations/20240620155645_cambiosAsistencias.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class cambiosAsistencias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Asistencias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fecha = table.Column<DateOnly>(type: "date", nullable: true),
+                    HoraEntrada = table.Column<TimeOnly>(type: "time", nullable: true),
+                    HoraSalida = table.Column<TimeOnly>(type: "time", nullable: true),
+                    Retardo = table.Column<int>(type: "int", nullable: true),
+                    Total = table.Column<int>(type: "int", nullable: true),
+                    Faltas = table.Column<int>(type: "int", nullable: true),
+                    EmpleadoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Asistencias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Asistencias_Empleados_EmpleadoId",
+                        column: x => x.EmpleadoId,
+                        principalTable: "Empleados",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Asistencias_EmpleadoId",
+                table: "Asistencias",
+                column: "EmpleadoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Asistencias");
+        }
+    }
+}
