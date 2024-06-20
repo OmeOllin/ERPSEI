5c216e4cb5343f9ab94d218b6209bd0f7f6af4ae
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-06-20 14:34:23 -0600

    Modificaciones campos Asistencia

--- ERPSEI/Data/Entities/Empleados/Asistencia.cs
+++ ERPSEI/Data/Entities/Empleados/Asistencia.cs
@@ -1,40 +1,22 @@
-﻿using ERPSEI.Data.Managers;
-using ERPSEI.Data.Managers.Empleados;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ERPSEI.Data.Entities.Empleados
 {
 	public class Asistencia 
 	{
-		[DatabaseGenerated(DatabaseGeneratedOption.None)]
-		public int Id { get; set; }
-		public DateTime? FechaHora { get; set; }
-		public DateOnly? Fecha { get; set; }
-		public TimeSpan? Hora { get; set; }
-		public string? Direccion { get; set; }
-		public string? NombreDispositivo { get; set; }
-	    public string? SerialDispositivo { get; set; }
-		public string? NombreEmpleado { get; set; }
-		public int? NoTarjeta { get; set; }
-
-		/*
-		public int Id { get; set; }
-		public string? Nombre { get; set; } = string.Empty;
-		public DateOnly? Fecha { get; set; }
-		public TimeOnly? HoraEntrada { get; set; }
-		public TimeOnly? HoraSalida { get; set; }
-		public int? Retardo { get; set; }
-		public int? Total { get; set; }
-		public int? Faltas { get; set; }
-		*/
+		[DatabaseGenerated(DatabaseGeneratedOption.None)] 
+		public string Id { get; set; } = string.Empty;
+		public DateTime FechaHora { get; set; }
+		public DateOnly Fecha { get; set; }
+		public TimeSpan Hora { get; set; }
+		public string Direccion { get; set; } = string.Empty;
+		public string NombreDispositivo { get; set; } = string.Empty;
+		public string SerialDispositivo { get; set; } = string.Empty;
+		public string NombreEmpleado { get; set; } = string.Empty;
+		public string NoTarjeta { get; set; } = string.Empty;
 
 		//Relaciones de la entidad
 		public int? EmpleadoId { get; set; }
 		public Empleado? Empleado { get; set; }
-
-		//[NotMapped]
-		//public List<Empleado>? Empleados { get; set; }
-		//public ICollection<Empleado>? Empleados { get; set; }
 	}
 }
