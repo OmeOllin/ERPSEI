8b7c54048c8eda54d7e5fb70d4fde7f00ab47986
JorgeAlbertoCruzPercastegui <jacp@soportecliente.com.mx>
2024-06-21 18:46:06 -0600

    Modificación y limpieza de codigo para solo lectura de asistencia

--- ERPSEI/Areas/Reportes/Pages/Asistencia.cshtml.cs
+++ ERPSEI/Areas/Reportes/Pages/Asistencia.cshtml.cs
@@ -1,11 +1,14 @@
 using ERPSEI.Data;
 using ERPSEI.Data.Entities.Empleados;
+using ERPSEI.Data.Entities.Usuarios;
 using ERPSEI.Data.Managers;
 using ERPSEI.Data.Managers.Empleados;
 using ERPSEI.Data.Managers.SAT;
 using ERPSEI.Requests;
 using ERPSEI.Resources;
+using ExcelDataReader.Log;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Localization;
@@ -45,15 +48,15 @@ namespace ERPSEI.Areas.Catalogos.Pages
 		public class Asistencia
 		{
 			[Display(Name = "Id")]
-			public int Id { get; set; }
-			public DateTime? FechaHora { get; set; }
-			public DateOnly? Fecha { get; set; }
-			public TimeSpan? Hora { get; set; }
-			public string? Direccion { get; set; }
-			public string? NombreDispositivo { get; set; }
-			public string? SerialDispositivo { get; set; }
-			public string? NombreEmpleado { get; set; }
-			public int? NoTarjeta { get; set; }
+			public string Id { get; set; } = string.Empty;
+			public DateTime FechaHora { get; set; }
+			public DateOnly Fecha { get; set; }
+			public TimeSpan Hora { get; set; }
+			public string Direccion { get; set; } = string.Empty;
+			public string NombreDispositivo { get; set; } = string.Empty;
+			public string SerialDispositivo { get; set; } = string.Empty;
+			public string NombreEmpleado { get; set; } = string.Empty;
+			public string NoTarjeta { get; set; } = string.Empty;
 		}
 		public AsistenciaModel(
 			ApplicationDbContext db,
@@ -73,21 +76,36 @@ namespace ERPSEI.Areas.Catalogos.Pages
 			ListaAsistencia = new Asistencia();
 		}
 
+		/*public JsonResult OnGetAsistenciaList()
+		{
+			List<Data.Entities.Empleados.Asistencia> asistencias = _asistenciaManager.GetAllAsync().Result;
+
+			return new JsonResult(asistencias);
+		}*/
 		public async Task<JsonResult> OnGetAsistenciasList()
 		{
-			string jsonResponse;
+			string jsonResponse = string.Empty;
+			List<Data.Entities.Empleados.Asistencia> asistencias = await _asistenciaManager.GetAllAsync();
 			List<string> jsonAsistencias = new List<string>();
-			List<Data.Entities.Empleados.Asistencia> asistencias = _asistenciaManager.GetAllAsync().Result; 
+			
 
 			foreach (Data.Entities.Empleados.Asistencia asis in asistencias)
 			{
 				// Construir el JSON para cada asistencia
-				string asistenciaJson = $"{{\"id\": {asis.Id}, \"nombre\": \"{asis.NombreEmpleado}\", \"FechaHora\": \"{asis.FechaHora}\", \"Fecha\": \"{asis.Fecha}\", \"Hora\": \"{asis.Hora}\", \"Dirección\": {asis.Direccion}, \"NombreDispositivo\": {asis.NombreDispositivo}, \"SerialDispositivo\": {asis.SerialDispositivo}, \"NoTarjeta\": {asis.NoTarjeta}}}";
-				jsonAsistencias.Add(asistenciaJson);
+				jsonAsistencias.Add("{" +
+					$"\"Id\": \"{asis.Id}\", " +
+					$"\"FechaHora\": \"{asis.FechaHora}\", " +
+					$"\"Fecha\": \"{asis.Fecha}\", " +
+					$"\"Hora\": \"{asis.Hora}\", " +
+					$"\"Direccion\": \"{asis.Direccion}\", " +
+					$"\"NombreDispositivo\": \"{asis.NombreDispositivo}\", " +
+					$"\"SerialDispositivo\": \"{asis.SerialDispositivo}\", " +
+					$"\"NombreEmpleado\": \"{asis.NombreEmpleado}\", " +
+					$"\"NoTarjeta\": \"{asis.NoTarjeta}\", " +
+					"}");
 			}
-
 			jsonResponse = $"[{String.Join(",", jsonAsistencias)}]";
 			return new JsonResult(jsonResponse);
-		}
+		} 
 	}
 }
--- ERPSEI/Areas/Reportes/Pages/Asistencia.cshtml.js
+++ ERPSEI/Areas/Reportes/Pages/Asistencia.cshtml.js
@@ -1,38 +1,22 @@
 ﻿var table;
-var buttonRemove;
 var selections = [];
-var dlg = null;
-var dlgModal = null;
-
-const NUEVO = 0;
-const EDITAR = 1;
-const VER = 2;
-const postOptions = { headers: { "RequestVerificationToken": $('input[name="__RequestVerificationToken"]').val() } }
 
 document.addEventListener("DOMContentLoaded", function (event) {
     table = $("#table");
-    buttonRemove = $("#remove");
-    dlg = document.getElementById('dlgAsistencia');
-    dlgModal = new bootstrap.Modal(dlg, null);
-    //Función para limpiar el cuadro de diálogo cuando es cerrado
-    dlg.addEventListener('hidden.bs.modal', function (event) {
-        onCerrarClick();
-    });
-
     initTable();
 });
 
-//Funcionalidad Tabla
 function getIdSelections() {
     return $.map(table.bootstrapTable('getSelections'), function (row) {
-        return row.id
-    })
+        return row.id;
+    });
 }
+
 function responseHandler(res) {
     $.each(res, function (i, row) {
-        row.state = $.inArray(row.id, selections) !== -1
-    })
-    return res
+        row.state = $.inArray(row.id, selections) !== -1;
+    });
+    return res;
 }
 function initTable() {
     table.bootstrapTable('destroy').bootstrapTable({
@@ -42,22 +26,22 @@ function initTable() {
         exportTypes: ['excel'],
         columns: [
             {
-                title: colNombreHeader,
-                field: "Nombre",
+                title: colIdHeader,
+                field: "Id",
                 align: "center",
                 valign: "middle",
                 sortable: true
             },
             {
                 title: colFechaHoraHeader,
-                field: "Fecha",
+                field: "FechaHora",
                 align: "center",
                 valign: "middle",
                 sortable: true
             },
             {
                 title: colFechaHeader,
-                field: "Hora",
+                field: "Fecha",
                 align: "center",
                 valign: "middle",
                 sortable: true
@@ -89,112 +73,28 @@ function initTable() {
                 align: "center",
                 valign: "middle",
                 sortable: true
+            },
+            {
+                title: colNombreEmpleadoHeader,
+                field: "NombreEmpleado",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: colNoTarjetaHeader,
+                field: "NoTarjeta",
+                align: "center",
+                valign: "middle",
+                sortable: true
             }
         ]
     })
-    table.on('check.bs.table uncheck.bs.table ' +
-        'check-all.bs.table uncheck-all.bs.table',
-        function () {
-            buttonRemove.prop('disabled', !table.bootstrapTable('getSelections').length)
+    table.on('check.bs.table uncheck.bs.table check-all.bs.table uncheck-all.bs.table', function () {
+        selections = getIdSelections();
+    });
 
-            // save your data, here just save the current page
-            selections = getIdSelections()
-            // push or splice the selections if you want to save all data selections
-        })
     table.on('all.bs.table', function (e, name, args) {
-        console.log(name, args)
-    })
-    buttonRemove.click(function () {
-        askConfirmation(dlgDeleteTitle, dlgDeleteQuestion, function () {
-            let oParams = { ids: selections };
-
-            doAjax(
-                "/Catalogos/Asistencias/DeleteAsistencias",
-                oParams,
-                function (resp) {
-                    if (resp.tieneError) {
-                        showError(dlgDeleteTitle, resp.mensaje);
-                        return;
-                    }
-
-                    table.bootstrapTable('remove', {
-                        field: 'id',
-                        values: selections
-                    })
-                    selections = [];
-                    buttonRemove.prop('disabled', true);
-
-                    let e = document.querySelector("[name='refresh']");
-                    e.click();
-
-                    showSuccess(dlgDeleteTitle, resp.mensaje);
-                }, function (error) {
-                    showError(dlgDeleteTitle, error);
-                },
-                postOptions
-            );
-
-        });
-    })
-}
-/////////////////////
-function onGuardarClick() {
-    // Ejecuta la validación
-    $("#theForm").validate();
-    // Determina los errores
-    let valid = $("#theForm").valid();
-    // Si la forma no es válida, entonces finaliza.
-    if (!valid) { return; }
-
-    let idField = document.getElementById("inpAsistenciaId");
-    let nombreField = document.getElementById("inpAsistenciaNombre");
-    let fechaHoraField = document.getElementById("inpAsistenciaFechaHora");
-    let fechaField = document.getElementById("inpAsistenciaFecha");
-    let horaField = document.getElementById("inpAsistenciaHora");
-    let direccionField = document.getElementById("inpAsistenciaDireccion");
-    let nombreDispositivoField = document.getElementById("inpAsistenciaNombreDispositivo");
-    let serialDispositivoField = document.getElementById("inpAsistenciaSerialDispositivo");
-    let noTarjetaField = document.getElementById("inpAsistenciaNoTarjeta");
-    let summaryContainer = document.getElementById("saveValidationSummary");
-    summaryContainer.innerHTML = "";
-
-    let oParams = {
-        id: idField.value == "Nuevo" ? 0 : idField.value,
-        nombre: nombreField.value,
-        fechaHora: fechaHoraField.value, 
-        fecha: fechaField.value,
-        hora: horaField.value,
-        direccion: direccionField.value,
-        nombreDispositivo: nombreDispositivoField.value,
-        serialDispositivo: serialDispositivoField.value,
-        noTarjeta: noTarjetaField.value
-    };
-
-    doAjax(
-        "/Catalogos/Asistencias/SaveAsistencia",
-        oParams,
-        function (resp) {
-            if (resp.tieneError) {
-                if (Array.isArray(resp.errores) && resp.errores.length >= 1) {
-                    let summary = ``;
-                    resp.errores.forEach(function (error) {
-                        summary += `<li>${error}</li>`;
-                    });
-                    summaryContainer.innerHTML += `<ul>${summary}</ul>`;
-                }
-                showError(dlgTitle.innerHTML, resp.mensaje);
-                return;
-            }
-
-            btnClose.click();
-
-            let e = document.querySelector("[name='refresh']");
-            e.click();
-
-            showSuccess(dlgTitle.innerHTML, resp.mensaje);
-        }, function (error) {
-            showError("Error", error);
-        },
-        postOptions
-    );
+        console.log(name, args);
+    });
 }
