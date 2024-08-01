36295dbfce2bbf4436ebec5900efc100dc1511d8
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-08-01 00:13:51 -0600

    Se agrega una clase base de la cual heredarán todos los modelos de página para poder tener acceso a las variables de permisos del módulo.

---
+++ ERPSEI/Pages/Shared/ERPPageModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERPSEI.Pages.Shared
+{
+	public class ERPPageModel : PageModel
+	{
+		protected bool PuedeTodo { get { return User.Claims.Where(c => c.Type.Equals("PuedeTodo", StringComparison.OrdinalIgnoreCase)).FirstOrDefault()?.Value == "1"; } }
+		protected bool PuedeConsultar { get { return User.Claims.Where(c => c.Type.Equals("PuedeConsultar", StringComparison.OrdinalIgnoreCase)).FirstOrDefault()?.Value == "1"; } }
+		protected bool PuedeEditar { get { return User.Claims.Where(c => c.Type.Equals("PuedeEditar", StringComparison.OrdinalIgnoreCase)).FirstOrDefault()?.Value == "1"; } }
+		protected bool PuedeEliminar { get { return User.Claims.Where(c => c.Type.Equals("PuedeEliminar", StringComparison.OrdinalIgnoreCase)).FirstOrDefault()?.Value == "1"; } }
+		protected bool PuedeAutorizar { get { return User.Claims.Where(c => c.Type.Equals("PuedeAutorizar", StringComparison.OrdinalIgnoreCase)).FirstOrDefault()?.Value == "1"; } }
+	}
+}
