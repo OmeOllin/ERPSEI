2838cbaebae40465bc2268d1c3de86316bbfd60c
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-08-23 16:59:23 -0600

    Se agrega migración para incluir firma electrónica a las empresas

---
+++ ERPSEI/Data/Migrations/20240823223450_firmaElectronicaEmpresas.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class firmaElectronicaEmpresas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PFESAT",
+                table: "Empresas",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Salida",
+                table: "Asistencias",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ResultadoS",
+                table: "Asistencias",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ResultadoE",
+                table: "Asistencias",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Fecha",
+                table: "Asistencias",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Dia",
+                table: "Asistencias",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "TipoArchivoEmpresa",
+                columns: new[] { "Id", "Description" },
+                values: new object[,]
+                {
+                    { 6, "CER" },
+                    { 7, "KEY" },
+                    { 8, "Logo" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TipoArchivoEmpresa",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "TipoArchivoEmpresa",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "TipoArchivoEmpresa",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DropColumn(
+                name: "PFESAT",
+                table: "Empresas");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Salida",
+                table: "Asistencias",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0),
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ResultadoS",
+                table: "Asistencias",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ResultadoE",
+                table: "Asistencias",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Fecha",
+                table: "Asistencias",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Dia",
+                table: "Asistencias",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
