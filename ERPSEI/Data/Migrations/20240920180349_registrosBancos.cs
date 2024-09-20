4861b806926582e6a71fbef739c4cf8f8b00ecda
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-09-20 12:04:37 -0600

    Se crea y aplica migración para añadir registros de bancos a la tabla Bancos

---
+++ ERPSEI/Data/Migrations/20240920180349_registrosBancos.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class registrosBancos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Bancos",
+                columns: new[] { "Id", "Deshabilitado", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, false, "Alquimia" },
+                    { 2, false, "Afirme" },
+                    { 3, false, "Azteca" },
+                    { 4, false, "American Express" },
+                    { 5, false, "Bancomer" },
+                    { 6, false, "Bancoppel" },
+                    { 7, false, "Banorte" },
+                    { 8, false, "Banregio" },
+                    { 9, false, "Bajio" },
+                    { 10, false, "Base" },
+                    { 11, false, "Bx" },
+                    { 12, false, "Cibanco" },
+                    { 13, false, "Citibanamex" },
+                    { 14, false, "Fortuna" },
+                    { 15, false, "HSBC" },
+                    { 16, false, "Inbursa" },
+                    { 17, false, "Intercam" },
+                    { 18, false, "Invex" },
+                    { 19, false, "Jeeves" },
+                    { 20, false, "Konfio" },
+                    { 21, false, "Mercado Pago" },
+                    { 22, false, "Mifel" },
+                    { 23, false, "Monex" },
+                    { 24, false, "Multiva" },
+                    { 25, false, "Santander" },
+                    { 26, false, "Scotiabank" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Bancos",
+                keyColumn: "Id",
+                keyValue: 26);
+        }
+    }
+}
